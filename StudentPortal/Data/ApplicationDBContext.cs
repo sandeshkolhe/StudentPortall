e3bf79a2531c04e5b52b93597d2ca2fe62cd9275
Sandesh Kolhe <sandykolhe98@gmail.com>
2024-06-28 16:22:27 +0530

    Add project files.

---
+++ StudentPortal/Data/ApplicationDBContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace StudentPortal.Data
+{
+    public class ApplicationDBContext:DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+            
+        }
+    }
+}
